da12ea2398747b5fce3387874243a12fcf867779
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-10 16:03:56 -0500

    Solution

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {count: 0};
+    this.counterUpdate = this.counterUpdate.bind(this)
+  }
+
+  counterUpdate(){
+    this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.counterUpdate}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
